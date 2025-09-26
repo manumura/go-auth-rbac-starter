61892575bdabc9a7188f41d539b9d67c0050306f
Emmanuel Mura <emmanuel.mura@gmail.com>
2025-09-26 15:45:50 +0700

    rate limiting

--- middleware/rate_limiting_middleware.go
+++ middleware/rate_limiting_middleware.go
@@ -10,10 +10,23 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var (
+	internalServerErrorResponse = gin.H{
+		"message":    "Internal server error",
+		"error":      "internal_server_error",
+		"statusCode": http.StatusInternalServerError,
+	}
+
+	rateLimitExceededResponse = gin.H{
+		"message":    "Rate limit exceeded. Try again later.",
+		"error":      "rate_limit_exceeded",
+		"statusCode": http.StatusTooManyRequests,
+	}
+)
+
 // RateLimitingMiddleware is a middleware that limits the number of requests per user/IP address.
 // It uses a sliding window log algorithm to track requests and enforce limits.
 // The middleware relies on a Redis cache to store request timestamps for each user/IP.
-// Sliding Window Log Algorithm https://raphaeldelio.com/2025/01/22/sliding-window-log-rate-limiter-redis-java/
 func RateLimitingMiddleware(redisClient *redis.Client, rateLimit int64, window time.Duration) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		userID := ctx.Request.Header.Get("X-User-ID")
@@ -24,7 +37,7 @@ func RateLimitingMiddleware(redisClient *redis.Client, rateLimit int64, window t
 		now := time.Now()
 		nowUnix := now.Unix()
 		key := "rate_limit:" + userID
-		log.Info().Msgf("RateLimitingMiddleware: userID=%s, key=%s\n", userID, key)
+		log.Info().Msgf("RateLimitingMiddleware: userID=%s, key=%s", userID, key)
 
 		// Start a Redis transaction
 		pipe := redisClient.TxPipeline()
@@ -51,27 +64,65 @@ func RateLimitingMiddleware(redisClient *redis.Client, rateLimit int64, window t
 		// Execute the transaction
 		_, err := pipe.Exec(ctx)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				"message":    "Internal server error",
-				"error":      "internal_server_error",
-				"statusCode": http.StatusInternalServerError,
-			})
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, internalServerErrorResponse)
 			return
 		}
 
 		requestCount := countCmd.Val()
-		log.Info().Msgf("RateLimitingMiddleware: requestCount=%d\n", requestCount)
+		log.Info().Msgf("RateLimitingMiddleware: requestCount=%d", requestCount)
 
 		if requestCount > rateLimit {
 			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Retry-After
 			ctx.Header("Retry-After", fmt.Sprintf("%d", int(window.Seconds())))
-			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
-				"message":    "Rate limit exceeded. Try again later.",
-				"error":      "rate_limit_exceeded",
-				"statusCode": http.StatusTooManyRequests,
-			})
+			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, rateLimitExceededResponse)
 			return
 		}
 		ctx.Next()
 	}
 }
+
+// Sliding Window Log Algorithm https://raphaeldelio.com/2025/01/22/sliding-window-log-rate-limiter-redis-java/
+// func RateLimitingMiddleware(redisClient *redis.Client, rateLimit int64, window time.Duration) gin.HandlerFunc {
+// 	return func(ctx *gin.Context) {
+// 		userID := ctx.Request.Header.Get("X-User-ID")
+// 		if userID == "" {
+// 			userID = ctx.Request.RemoteAddr
+// 		}
+
+// 		now := time.Now()
+// 		key := "rate_limit:" + userID
+// 		log.Info().Msgf("RateLimitingMiddleware: userID=%s, key=%s", userID, key)
+
+// 		requestCount, err := redisClient.HLen(ctx, key).Result()
+// 		if err != nil {
+// 			log.Error().Err(err).Msg("RateLimitingMiddleware: Redis HLen failed")
+// 			ctx.AbortWithStatusJSON(http.StatusInternalServerError, internalServerErrorResponse)
+// 			return
+// 		}
+// 		log.Info().Msgf("RateLimitingMiddleware: requestCount=%d", requestCount)
+
+// 		if requestCount > rateLimit {
+// 			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Retry-After
+// 			ctx.Header("Retry-After", fmt.Sprintf("%d", int(window.Seconds())))
+// 			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, rateLimitExceededResponse)
+// 			return
+// 		}
+
+// 		// Start a Redis transaction
+// 		pipe := redisClient.TxPipeline()
+
+// 		value := fmt.Sprintf("%d", now.UnixNano())
+// 		pipe.HSet(ctx, key, value, "")
+// 		pipe.HExpire(ctx, key, window, value) // available only since valkey 9.0.0
+
+// 		// Execute the transaction
+// 		_, err = pipe.Exec(ctx)
+// 		if err != nil {
+// 			log.Error().Err(err).Msg("RateLimitingMiddleware: Redis transaction failed")
+// 			ctx.AbortWithStatusJSON(http.StatusInternalServerError, internalServerErrorResponse)
+// 			return
+// 		}
+
+// 		ctx.Next()
+// 	}
+// }
